7245757bb8905eefe869540e2e1843e0250f3432
IlBiK891 <ilbik831@yandex.ru>
2025-01-03 17:58:24 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,17 @@ public class Main {
         numberInput.closeScanner();
         if (input > multitude.size()){
             System.out.println("Пустое множество");
-        }
-        Combination combination = new Combination(multitude, input);
-        System.out.println("Все комбинации:");
-        for (int i = 0; i < combination.finish.length; i += input) {
-            // Вывод элементов одной комбинации
-            for (int j = 0; j < input; j++) {
-                System.out.print(combination.finish[i + j] + " ");
+        }else {
+            Combination combination = new Combination(multitude, input);
+            System.out.println("Все комбинации:");
+            for (int i = 0; i < combination.finish.length; i += input) {
+                // Вывод элементов одной комбинации
+                for (int j = 0; j < input; j++) {
+                    System.out.print(combination.finish[i + j] + " ");
+                }
+                System.out.println(); // Перевод строки
             }
-            System.out.println(); // Перевод строки
         }
     }
+
 }
