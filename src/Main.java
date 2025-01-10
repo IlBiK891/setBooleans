251aad64abc57f5c1e70440f2c2974376f9ac3ef
IlBiK891 <146674110+IlBiK891@users.noreply.github.com>
2025-01-10 13:00:05 +0300

    Update1 Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,8 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // Создаём обработчик ввода
         InputHandler inputHandler = new InputHandler();
 
-        // Ввод множества
         Set<String> set = inputHandler.getSetFromUser();
 
         // Ввод порядка булеана
@@ -14,11 +12,9 @@ public class Main {
         // Генерация булеана n-го порядка
         List<?> nthPowerSet = NthPowerSet.generateNthPowerSet(set, order);
 
-        // Вывод результата
-        System.out.println("Булеан " + order + "-го порядка: ");
+       System.out.println("Булеан " + order + "-го порядка: ");
         System.out.println(nthPowerSet);
 
-        // Закрываем сканер
         inputHandler.closeScanner();
     }
 }
